1a762c4e4305ab2818789d7a3c4b5ef705e142c9
Huan Wang <huan.w.wang@gmail.com>
2021-07-19 18:03:25 +0000

    pushing to test heroku

--- app.py
+++ app.py
@@ -2,15 +2,30 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
-import os
+from model import getImageUrlFrom
+import os #import os allows us to access environmental variables that 
 
 # -- Initialization section --
 app = Flask(__name__)
+#need to pull API Key from .env for later use
+#the .env file with the key does not get pushed to Github since it is in .gitignore file
+app.config["GIPHY_KEY"] = os.getenv("GIPHY_KEY")
 
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
+#time = datetime.now() is a way to trick your browser into not caching CSS and reloading the CSS every time
 def index():
     return render_template("index.html", time = datetime.now())
+
+@app.route('/yourgif', methods=['GET', 'POST'])
+def yourgif():
+    #needs to pull form data from index.html (name was gifchoice)
+    query = request.form["gifchoice"]
+    #this print statement is just for debugging purposes
+    print(request.form)
+    #needs to call getImageUrlFrom function, passing in query as an argument
+    gif_choice = getImageUrlFrom(query, app.config["GIPHY_KEY"])
+    #needs to pass URL into render_template
+    return render_template("yourgif.html", time = datetime.now(), image = gif_choice)
--- model.py
+++ model.py
@@ -1,2 +1,19 @@
-def getImageUrlFrom(query):
-    pass
+import requests
+
+#this function should return a string that is the link to the giphy image based on user input (query)
+def getImageUrlFrom(query, key):
+    #GIPHY search URL with dynamic query 
+    #(f in front of string in Python creates a formatted string that you can include variables with in {}.
+    #This is potentially cleaner then using the + for concantenation)
+    giphy_get = f"https://api.giphy.com/v1/gifs/search?api_key={key}&q={query}&limit=25&offset=0&rating=g&lang=en"
+    #converts data into json data structure
+    response = requests.get(giphy_get).json()
+    #if condition returns first giphy result if there is at least one result
+    if len(response["data"]) > 0:
+    #url after analyzing nested json structure of response
+        url = response["data"][0]["images"]["fixed_height"]["url"]
+        return url
+    #else condition deals with possibility user query returns no GIPHY results
+    else:
+        no_results_img_link = "https://unbxd.com/wp-content/uploads/2014/02/No-results-found.jpg"
+        return no_results_img_link
